9f56f1edd10c85dda2665625aec8f0d3254fed84
Igor1py <grigorevigor2001@mail.ru>
2020-10-26 12:08:36 +0300

    Fixed key errors in users DB

--- server/database/users.py
+++ server/database/users.py
@@ -10,7 +10,7 @@ class Users(Table):
         VALUES
         (%(login)s, %(password)s)
         RETURNING id
-        ''', kwargs)['id']
+        ''', kwargs).get('id')
 
     def login(self, **kwargs):
         return self._database.execute_with_returning('''
@@ -19,13 +19,13 @@ class Users(Table):
         WHERE login = %(login)s
         AND password = %(password)s
         RETURNING token
-        ''', kwargs)['token']
+        ''', kwargs).get('token')
 
     def get_user_id(self, **kwargs):
         return self._database.fetch_one('''
         SELECT user_id FROM tokens
         WHERE token = %(token)s
-        ''', kwargs)['user_id']
+        ''', kwargs).get('user_id')
 
     def logout(self, **kwargs):
         self._database.execute_and_commit('''

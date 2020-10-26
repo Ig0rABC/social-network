292f8e03e68ac31d574349cf9c48630d3d2f2221
Igor1py <grigorevigor2001@mail.ru>
2020-10-26 12:07:26 +0300

    Catch some DB errors

--- server/database/database.py
+++ server/database/database.py
@@ -1,5 +1,6 @@
 import psycopg2
 from psycopg2.extras import RealDictCursor
+from psycopg2.errors import UniqueViolation, ProgrammingError
 
 class Database:
     
@@ -8,26 +9,32 @@ class Database:
         self._cursor = self._connection.cursor(
             cursor_factory=RealDictCursor
         )
+
+    def rollback(self):
+        self._connection.rollback()
     
     def execute(self, query, params={}):
-        self._cursor.execute(query, params)
+        try:
+            self._cursor.execute(query, params)
+        except UniqueViolation:
+            self.rollback()
 
     def execute_and_commit(self, query, params={}):
         self.execute(query, params)
         self._connection.commit()
     
-    def execute_with_returning(self, query, params={}):
-        value = self.fetch_one(query, params)
-        self._connection.commit()
-        return value
-    
     def fetch_one(self, query, params={}):
         self.execute(query, params)
-        return self._cursor.fetchone()
+        try:
+            return self._cursor.fetchone()
+        except ProgrammingError:
+            return {}
     
     def fetch_all(self, query, params={}):
         self.execute(query, params)
         return self._cursor.fetchall()
-
-    def rollback(self):
-        self._connection.rollback()
+    
+    def execute_with_returning(self, query, params={}):
+        value = self.fetch_one(query, params)
+        self._connection.commit()
+        return value
